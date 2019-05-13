b6669935cc40d09292f87fd5cd2414d6e6bf365c
Pedro Veronezi <pveronezi@localhost.localdomain>
2019-05-13 09:11:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from hello import hello
+from calculator import calc
+
+def main():
+    hello.hello_world("pveronezi")
+    print('sum 2 numbers: {}'.format(calc.sum(3, 4)))
+
+
+if __name__ == '__main__':
+    main()
