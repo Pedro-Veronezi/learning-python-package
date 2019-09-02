256993d02366578c36219d53f3be1bf65434184f
pveronezi <pedroveronezi@gmail.com>
2019-09-02 16:32:59 -0300

    solved argparse

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from hello import hello
 from calculator import calc
+import argparse
 
+print("START")
+parser = argparse.ArgumentParser(description = "For Cloud ML Engine")
+
+parser.add_argument('--train_data_paths', action = 'store',dest = 'train_data_paths', required=True)
+parser.add_argument('--train_steps')
+args = parser.parse_args()
+print(type(args))
 def main():
-    hello.hello_world("pveronezi")
+    print(args.train_data_paths)
+    # hello.hello_world(args['train_data_paths'])
     print('sum 2 numbers: {}'.format(calc.sum(3, 4)))
 
 
 if __name__ == '__main__':
     main()
 
-# TODO: commit to master, then publish again. 
+# TODO: commit to master, then publish again.
